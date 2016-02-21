622335219536248e7a519ddbe1629644be833114
Torsten Martinsen <torsten@bullestock.net>
2016-02-21 22:52:50 +0100

    Set steps per mm for TB6600 drivers.

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -30,6 +30,7 @@
 #define DRIVEBOARD
 
 #define V1401
+#define TB6600
 
 #define BAUD_RATE 57600
 // #define DEBUG_IGNORE_SENSORS  // set for debugging
@@ -40,9 +41,17 @@
   #define CONFIG_Y_STEPS_PER_MM 32.80839895 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
   #define CONFIG_Z_STEPS_PER_MM 32.80839895 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
 #else
-  #define CONFIG_X_STEPS_PER_MM 88.88888888 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
-  #define CONFIG_Y_STEPS_PER_MM 90.90909090 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
-  #define CONFIG_Z_STEPS_PER_MM 33.33333333 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+#  ifdef TB6600
+// TB6600: 16 microsteps
+#    define CONFIG_X_STEPS_PER_MM 107.095046843 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+#    define CONFIG_Y_STEPS_PER_MM 109.529025181 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+#    define CONFIG_Z_STEPS_PER_MM 33.33333333 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+#  else
+  // Geckodrive
+#    define CONFIG_X_STEPS_PER_MM 88.88888888 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+#    define CONFIG_Y_STEPS_PER_MM 90.90909090 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+#    define CONFIG_Z_STEPS_PER_MM 33.33333333 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+#  endif
 #endif
 #define CONFIG_PULSE_MICROSECONDS 5
 #define CONFIG_FEEDRATE 8000.0 // in millimeters per minute
